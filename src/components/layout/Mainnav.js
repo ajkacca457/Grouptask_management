befe37944636aa69c25020fa037cf56fcefdcfd7
Avijit Karmaker <ajkacca@gmail.com>
2021-06-10 23:15:13 +0300

    fix navbar

--- src/components/layout/Mainnav.js
+++ src/components/layout/Mainnav.js
@@ -3,15 +3,18 @@ import React from 'react'
 const Mainnav=()=> {
     return (
         <>
- <nav className="blue" style={{padding:"5px"}}>
-    <div className="nav-wrapper">
-      <a href="#!" className="brand-logo"><img src="https://img.icons8.com/plasticine/40/000000/business-group.png"/></a>
-      <ul className="right hide-on-med-and-down">
-        <li><a href="sass.html"><img src="https://img.icons8.com/officel/16/000000/home.png" style={{marginRight:"5px"}}/>Home</a></li>
-        <li><a href="badges.html"><img src="https://img.icons8.com/fluent/16/000000/about.png" style={{marginRight:"5px"}}/>About</a></li>
-      </ul>
-    </div>
-  </nav>
+ <div className="blue" style={{padding:"5px"}}>
+        <div className="container" style={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
+        <div className="logo" style={{display:"flex",alignItems:"center"}}>
+        <img src="https://img.icons8.com/color/48/000000/football-team.png"/>
+        <h6 className="white-text text-white" style={{marginLeft:"10px",fontWeight:"bold"}}>GroupTask Manager</h6>
+        </div>
+        <div className="nav-links" style={{display:"flex", alignItems:"center"}}>
+        <a href="#" style={{color:"white", marginRight:"10px", display:"flex", alignItems:"center"}}><img src="https://img.icons8.com/flat-round/20/000000/home.png"/><p style={{marginLeft:"5px"}}>Home</p></a>
+        <a href="#" style={{color:"white",marginRight:"10px", display:"flex", alignItems:"center"}}><img src="https://img.icons8.com/fluent/20/000000/high-importance.png"/><p>About</p></a>
+        </div>
+        </div>        
+  </div>
     </>
     )
 }
